d3f44ea77edb8fd983c4941b0a148248716f8cb6
chellojuramu <chelloju@outlook.com>
2026-03-06 15:02:25 +0530

    feat(infra): attach IAM instance profile to MySQL instance using conditional logic in for_each resource

--- infra/40-databases/bootstrap.sh
+++ infra/40-databases/bootstrap.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# 1. Capture variables from Terraform
 component=$1
 environment=$2
+
+# 2. Log start time (Senior DevOps Best Practice for debugging)
+echo "Starting bootstrap for ${component} in ${environment} at $(date)"
+
+# 3. Install Ansible
 dnf install ansible -y
 
+# 4. Setup Workspace
 cd /home/ec2-user
-git clone https://github.com/chellojuramu/ansible-roboshop-roles-tf.git
+# Use conditional cloning so it doesn't fail if the folder already exists
+if [ ! -d "ansible-roboshop-roles-tf" ]; then
+    git clone https://github.com/chellojuramu/ansible-roboshop-roles-tf.git
+fi
 
 cd ansible-roboshop-roles-tf
 git pull
 
-ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
+# 5. Execute Playbook with Explicit Variables
+# Note: We use double quotes around variables to prevent shell splitting errors
+ansible-playbook -e "component=${component}" -e "environment=${environment}" roboshop.yaml
