8f2fd80cbf6b5711af56a42574459b8f29774a2e
chellojuramu <chelloju@outlook.com>
2026-03-06 16:53:51 +0530

    feat(infra): attach IAM instance profile to MySQL instance using conditional logic in for_each resource

--- infra/40-databases/bootstrap.sh
+++ infra/40-databases/bootstrap.sh
@@ -6,7 +6,7 @@ environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
-git clone https://github.com/daws-88s/ansible-roboshop-roles-tf.git
+git clone https://github.com/chellojuramu/ansible-roboshop-roles-tf
 
 cd ansible-roboshop-roles-tf
 git pull
