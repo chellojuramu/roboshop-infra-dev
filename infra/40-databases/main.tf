6464e110dd1c1b4027efd208f1ce9596c8246c25
chellojuramu <chelloju@outlook.com>
2026-03-06 16:51:17 +0530

    feat(infra): attach IAM instance profile to MySQL instance using conditional logic in for_each resource

--- infra/40-databases/main.tf
+++ infra/40-databases/main.tf
@@ -37,7 +37,7 @@ resource "terraform_data" "bootstrap" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh ${each.key}"
+      "sudo sh /tmp/bootstrap.sh ${each.key} ${var.environment}"
     ]
   }
 }
